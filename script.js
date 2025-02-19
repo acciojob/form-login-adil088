69bcab16b656244dc9432ec372011e7248875144
Mohd Adil <90774182+adil088@users.noreply.github.com>
2025-02-19 11:01:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	const fname = document.getElementById().value;
+	const lname = document.getElementById().value;
 
+	alert(fname);
+	alert(lname);
 }
