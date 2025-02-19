d7ce7bac32a0c6014d67f30cc36d24ba2d44c81e
Mohd Adil <90774182+adil088@users.noreply.github.com>
2025-02-19 11:04:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
+	event.preventDefault(); 
 	const fname = document.getElementById().value;
 	const lname = document.getElementById().value;
 
